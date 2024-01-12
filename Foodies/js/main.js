bba7bbd699b024dd690060544749e48842ded1b6
Vaishnavi Virkud <84440401+VaishnaviVirkud@users.noreply.github.com>
2024-01-12 20:52:29 +0530

    Add files via upload

---
+++ Foodies/js/main.js
@@ -0,0 +1,44 @@
+//active navbar
+
+let nav = document.querySelector(".navgation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+//nav hide
+
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+//counter design
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 1288, 3000);   //count1 is id from html code
+    counter("count2", 100, 500, 2500);
+    counter("count3", 0, 1580, 3000);
+    counter("count4", 0, 250, 3000);
+    
+});
